a022ad529adf1b9b8c6861ebc90a328d55038f7b
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2020-01-22 22:48:06 +0100

    compile_all small display fix

--- compile_all.sh
+++ compile_all.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+echo -e "\n[*] Starting Normal building...\n"
 cmake .
 make
 mv bin/Silent_Pass bin/Silent_Pass_x64
 ./clean.sh
 
+echo -e "\n[*] Starting 32 bit building...\n"
 cmake -DCMAKE_TOOLCHAIN_FILE=platforms/linux/linux_i686.toolchain.cmake .
 make
 mv bin/Silent_Pass bin/Silent_Pass_x32
 ./clean.sh
 
+echo -e "\n[*] Starting MinGW building...\n"
 cmake -DCMAKE_TOOLCHAIN_FILE=platforms/linux/mingw32.toolchain.cmake .
 make
 ./clean.sh
